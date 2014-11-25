052246c8385f0a7bedbadf13e7181ac1a29b71b8
codeinsect <codeinsect@126.com>
2014-11-25 16:36:27 +0800

    2014-11-25-2

---
+++ ThinkPrint/ThinkPrint/TP.Service/SysResource/IResourceService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.EntityFramework.Models;
+using Webdiyer.WebControls.Mvc;
+
+namespace TP.Service.SysResource
+{
+    public interface IResourceService
+    {
+        /// <summary>
+        /// 获取系统参数信息
+        /// </summary>
+        /// <returns></returns>
+        IList<SYS_SysSetting> GetSysSettingList();
+
+        /// <summary>
+        /// 分页获取系统参数信息
+        /// </summary>
+        /// <param name="pageIndex">页索引</param>
+        /// <param name="pageSize">每页显示数量</param>
+        /// <param name="searchKey">可选,名称</param>
+        /// <returns>PagedList SYS_SysSetting</returns>
+        PagedList<SYS_SysSetting> GetSysSettingList(int pageIndex, int pageSize, string searchKey = null);
+
+        /// <summary>
+        /// 根据标识编码获取系统参数信息列表
+        /// </summary>
+        /// <param name="titleCode">标识编码</param>
+        /// <returns></returns>
+        List<SYS_SysSetting> GetSysSettingList(string titleCode);
+
+
+        /// <summary>
+        /// 获取系统参数信息
+        /// </summary>
+        /// <param name="uniqueCode"></param>
+        /// <returns>SYS_SysSetting</returns>
+        SYS_SysSetting GetSysSettingByCode(string uniqueCode);
+
+        /// <summary>
+        /// 检查系统参数信息编码唯一性
+        /// </summary>
+        /// <param name="uniqueCode"></param>
+        /// <returns></returns>
+        SYS_SysSetting CheckExistSysSettingByCode(string uniqueCode);
+
+        /// <summary>
+        /// 添加系统参数信息
+        /// </summary>
+        /// <param name="sysSetting">sysSetting</param>
+        void InsertSysSetting(SYS_SysSetting sysSetting);
+
+        /// <summary>
+        /// 编辑系统参数信息
+        /// </summary>
+        /// <param name="sysSetting">sysSetting</param>
+        void UpdateSysSetting(SYS_SysSetting sysSetting);
+
+        /// <summary>
+        /// 删除系统参数信息
+        /// </summary>
+        /// <param name="sysSetting"></param>
+        void DeleteSysSetting(SYS_SysSetting sysSetting);
+    }
+}
---
+++ ThinkPrint/ThinkPrint/TP.Service/SysResource/ResourceService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.Core.UnitOfWork;
+using TP.EntityFramework.Models;
+using TP.Repository;
+using Webdiyer.WebControls.Mvc;
+
+namespace TP.Service.SysResource
+{
+    public class ResourceService : IResourceService
+    {
+         private readonly ISysSettingRepository _sysSettingRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public ResourceService(ISysSettingRepository sysSettingRepository, IUnitOfWork unitOfWork)
+        {
+            _sysSettingRepository = sysSettingRepository;
+            _unitOfWork = unitOfWork;
+
+        }
+
+        public IList<SYS_SysSetting> GetSysSettingList()
+        {
+            var query = _sysSettingRepository.Table;
+            IList<SYS_SysSetting> sysSettingList = query.OrderByDescending(u => u.ModifiedDate).ToList();
+            return sysSettingList;
+        }
+
+        public PagedList<SYS_SysSetting> GetSysSettingList(int pageIndex, int pageSize, string searchKey = null)
+        {
+            var query = _sysSettingRepository.Table;
+            if (!string.IsNullOrWhiteSpace(searchKey))
+            {
+                query = query.Where(u => u.Name.Contains(searchKey) || u.Title.Contains(searchKey));
+            }
+
+            query = query.OrderByDescending(u => u.ModifiedDate);
+
+            PagedList<SYS_SysSetting> sysSettingList = query.ToPagedList(pageIndex, pageSize);
+            return sysSettingList;
+        }
+
+        public List<SYS_SysSetting> GetSysSettingList(string titleCode)
+        {
+            var query = _sysSettingRepository.Filter(s => s.TitleCode == titleCode).ToList();
+            return query;
+        }
+
+        public SYS_SysSetting GetSysSettingByCode(string uniqueCode)
+        {
+            if (string.IsNullOrWhiteSpace(uniqueCode))
+                throw new ArgumentNullException("Get UniqueCode is Null");
+            var query = _sysSettingRepository.Filter(u => u.UniqueCode == uniqueCode).SingleOrDefault();
+            return query;
+        }
+
+        public SYS_SysSetting CheckExistSysSettingByCode(string uniqueCode)
+        {
+            if (string.IsNullOrWhiteSpace(uniqueCode))
+                throw new ArgumentNullException("Check UniqueCode is Null");
+            var query = _sysSettingRepository.Filter(u => u.UniqueCode == uniqueCode).FirstOrDefault();
+            return query;
+        }
+
+        public void InsertSysSetting(SYS_SysSetting sysSetting)
+        {
+            if (sysSetting == null)
+                throw new ArgumentNullException("Insert SYS_SysSetting Entity Is Null");
+
+            try
+            {
+                _sysSettingRepository.Add(sysSetting);
+                _unitOfWork.Commint();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void UpdateSysSetting(SYS_SysSetting sysSetting)
+        {
+            if (sysSetting == null)
+                throw new ArgumentNullException("Update SYS_SysSetting Entity Is Null");
+            try
+            {
+
+                _sysSettingRepository.Update(sysSetting);
+                _unitOfWork.Commint();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void DeleteSysSetting(SYS_SysSetting sysSetting)
+        {
+            if (sysSetting == null)
+                throw new ArgumentNullException("Delete SYS_SysSetting Entity is Null");
+            try
+            {
+                sysSetting.IsDelete = true;
+                UpdateSysSetting(sysSetting);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
