94bab762301df1796c483d5284e4bdca01b90937
codeinsect <codeinsect@126.com>
2014-11-26 11:00:25 +0800

    2014-11-26-4

---
+++ ThinkPrint/ThinkPrint/TP.Site/Controllers/ResourceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.EntityFramework.Models;
+using TP.Service.SysResource;
+using TP.Site.Helper;
+using TP.Site.Models.SysSetting;
+using Webdiyer.WebControls.Mvc;
+
+namespace TP.Site.Controllers
+{
+    public class ResourceController : Controller
+    {
+        private readonly IResourceService _resourceService;
+        public ResourceController(IResourceService resourceService)
+        {
+            _resourceService = resourceService;
+        }
+
+        // GET: Resource
+        public ActionResult Index(int pageIndex = 1, string searchKey = null)
+        {
+            searchKey = searchKey == null ? searchKey : searchKey.Trim();
+            PagedList<SYS_SysSetting> sysSettingList = _resourceService.GetSysSettingList(pageIndex, SysConstant.Page_PageSize, searchKey);
+
+            ResourceListModel model = new ResourceListModel();
+            model.ViewList = sysSettingList;
+            model.PageTitle = "系统参数";
+            model.PageSubTitle = "查看和维护系统中重要参数信息";
+            return View(model);
+           
+        }
+    }
+}
--- ThinkPrint/ThinkPrint/TP.Site/Models/SysSetting/ResourceViewModels.cs
+++ ThinkPrint/ThinkPrint/TP.Site/Models/SysSetting/ResourceViewModels.cs
@@ -8,20 +8,43 @@ using TP.Web.Framework.Mvc;
 
 namespace TP.Site.Models.SysSetting
 {
-    public class ResourceModel :BaseViewModel
+    public class ResourceModel : BaseViewModel
     {
         public ResourceModel()
         {
 
         }
 
-        [Required(ErrorMessage = "请输入员工名称")]
-        [StringLength(20, ErrorMessage = "员工名称过长.")]
-        [Display(Name = "员工名称")]
+        [Required(ErrorMessage = "请输入参数名称")]
+        [StringLength(20, ErrorMessage = "参数名称过长.")]
+        [Display(Name = "参数名称")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "请输入参数标识")]
+        [StringLength(20, ErrorMessage = "参数标识过长.")]
+        [Display(Name = "参数标识")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "请输入标识编码")]
+        [StringLength(20, ErrorMessage = "标识编码过长.")]
+        [Display(Name = "标识编码")]
+        public string TitleCode { get; set; }
+
+        [Required(ErrorMessage = "请输入参数编码")]
+        [StringLength(20, ErrorMessage = "参数编码过长.")]
+        [Display(Name = "参数编码")]
+        public string UniqueCode { get; set; }
+
+        [Required(ErrorMessage = "请输入参数值")]
+        [StringLength(50, ErrorMessage = "参数值过长.")]
+        [Display(Name = "参数值")]
+        public string ParamValue { get; set; }
+        public System.DateTime ModifiedDate { get; set; }
+
+        public string Description { get; set; }
     }
-    public class ResourceListModel : BaseListViewModel<SYS_SysSetting> 
+    public class ResourceListModel : BaseListViewModel<SYS_SysSetting>
     {
-        
+
     }
 }
